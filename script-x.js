57a7b248e85ff69ccf0b2a1fd296d48b8a61380f
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-02-10 23:40:35 -0800

    finished forecast display. needs css

--- script-x.js
+++ script-x.js
@@ -1,25 +1,27 @@
-
 const searchForm = $(`#search-form`)
 const searchInput = $(`#search-input`)
 const searchButton = $(`#search-button`)
 const currentCityStats = $(`#current-city-stats`)
 const historyDiv = $(`#history-div`)
+const forecastDiv = $(`#forecast-div`)
 var cityImageURLs = []
 var searchHistory = []
+var apk, currentCity, forecast;
 
 if(JSON.parse(localStorage.getItem(`weather-app-search-history`)) !== null){
     searchHistory = JSON.parse(localStorage.getItem(`weather-app-search-history`))
 }
 
-var currentCity;
 
 var weatherErr = function weatherError(){
     searchInput.attr(`style`, `border-style: groove; border-color: red;`).attr(`placeholder`,`ENTER VALID CITY NAME`)
+    // searchHistory.splice()      // want to delete the last item from array if returned with error
 }
 
 init()
 
 function init(){
+    // may want to pull local GPS data provided by browser in future release
     searchTerm = `sacramento`
     currentCity = {}
     pullWeatherData()
@@ -27,20 +29,20 @@ function init(){
 }
 function printHistory(){        // find how to make the list collapsible on sm screens (with classes)
     historyDiv.empty()
-    var h4 = $(`<p class="lead">`).text(`Search History:`)
-    historyDiv.append(h4)
-    console.log(searchHistory.length)
+    var para = $(`<p class="lead">`).text(`Search History:`)
+    historyDiv.append(para)
     var x = 0
+    // if searchHistory has more than 10 entries only display the latest 10, otherwise display all.
     if (searchHistory.length > 10) {
         x = searchHistory.length - 10
     } else if (searchHistory.length === 0) {
         historyDiv.append($(`<p style="font-size:small">`).text(`(No search history)`))
     }
+    // loop through searchHistory array and make list item for each
+    var ul = $(`<ul class="pl-1 text-left" id="history-list">`)
     for (i = searchHistory.length - 1; i > x; i--) {
-        var ul = $(`<ul class="pl-2 w-100" id="history-list">`)
-        $(`<li class="bg-info">`).text(searchHistory[i]).appendTo(ul)
+        $(`<li class="history-list-items p-1 nav-item text-truncate">`).attr(`data-isListItem`,`y`).text(searchHistory[i]).appendTo(ul)
         historyDiv.append(ul)
-
     }
 }
 searchForm.submit(function(event){
@@ -53,10 +55,10 @@ searchForm.submit(function(event){
     printHistory()
 })
 function pullWeatherData(){
-    var apiKey = `a49df28db557e1be1c568c4992f04aec`
-    
+    apk = `a49df28db557e1be1c568c4992f04aec`
+
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q="+searchTerm+"&appid="+apiKey+"&units=imperial",
+        url: "https://api.openweathermap.org/data/2.5/weather?q="+searchTerm+"&appid="+apk+"&units=imperial",
         method: "GET",
         error: weatherErr
     }).then(function(response){
@@ -76,7 +78,7 @@ function pullWeatherData(){
     }).then(function(){
         var ak = `AIzaSyAYlOOGrSdKUf7eEO-W37dOfQJBAbImqvY`
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/uvi?lat="+currentCity.coord.lat+"&lon="+currentCity.coord.lon+"&appid="+apiKey,
+            url: "https://api.openweathermap.org/data/2.5/uvi?lat="+currentCity.coord.lat+"&lon="+currentCity.coord.lon+"&appid="+apk,
             method: "GET",
             error: printWeatherData(),
         }).then(function(res){
@@ -96,8 +98,39 @@ function pullWeatherData(){
 
         console.log(`===== WEATHER RESPONSE END =====`)
     })
-    
-    console.log(currentCity)
+
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast?q="+searchTerm+"&appid="+apk+"&units=imperial",
+        method: "GET",
+        error: weatherErr
+    }).then(function(fresponse){
+        console.log(`===== FORECAST RESPONSE (RECEIVED) START =====`)
+        searchInput.attr(`style`, ``)
+        console.log(fresponse)
+        currentCity.forecast = []
+        
+        var g = 0
+        for(f=7;f<fresponse.list.length;f+=7){
+            currentCity.forecast[g] = {}
+
+            currentCity.forecast[g].temp = fresponse.list[f].main.temp + " F°"
+            currentCity.forecast[g].date = fresponse.list[f].dt
+            currentCity.forecast[g].humidity = fresponse.list[f].main.humidity + " %"
+            currentCity.forecast[g].icon_url = `http://openweathermap.org/img/wn/`+fresponse.list[f].weather[0].icon+`@2x.png`
+            g++
+        }
+
+        // currentCity.name = response.name
+        // forecast.temp = response.main.temp + " F°"
+        // currentCity.humidity = response.main.humidity + " %"
+        // currentCity.windSpeed = response.wind.speed + " MPH"
+        // currentCity.coord = response.coord
+        // currentCity.icon_url = `http://openweathermap.org/img/wn/`+response.weather[0].icon+`@2x.png`
+        // currentCity.timezoneGMT = Math.floor(((response.timezone)/60))
+        // console.log(currentCity.timezoneGMT)
+        // console.log(`local time in ` + currentCity.name + ` is ` + moment().utcOffset(currentCity.timezoneGMT).format(`ll`))
+    }).then(printForecastData)
+
 }
 function printWeatherData(){
     
@@ -111,43 +144,55 @@ function printWeatherData(){
     var currentUVIndex = $(`<li>`).attr(`class`,`p-1 list-group-item rounded-0 bg-transparent`).attr(`id`,`city-uvin`).html(`<span class="font-weight-bold">UV Index</span>: `+currentCity.uvIndex)
     var currentWindspeed = $(`<li>`).attr(`class`,`p-1 list-group-item rounded-0 bg-transparent`).attr(`id`,`city-wind`).html(`<span class="font-weight-bold">Wind Speed</span>: `+currentCity.windSpeed)
    
-    switch(Math.floor(currentCity.uvIndex)){
-        case 0:
-        case 1:
-        case 2:
-            currentUVIndex.attr(`class`, `p-1 list-group-item rounded-0 bg-suc`)
-            break
-        case 3:
-        case 4:
-        case 5:
-            currentUVIndex.attr(`class`, `p-1 list-group-item rounded-0 bg-war`)
-            break
-        case 6:
-        case 7:
-            currentUVIndex.attr(`class`,`p-1 list-group-item rounded-0 bg-ora`)
-            break
-        case 8:
-        case 9:
-        case 10:
-            currentUVIndex.attr(`class`,`p-1 list-group-item rounded-0 bg-dan`)
-            break
-        default:
-            currentUVIndex.attr(`class`,`p-1 list-group-item rounded-0 bg-vio`)
-            break
-    }
+    var uvin = Math.floor(currentCity.uvIndex)
+    if(uvin > 11){
+        currentUVIndex.attr(`class`,`p-1 list-group-item rounded-0 bg-vio`)
+    } else if (uvin >= 8){
+        currentUVIndex.attr(`class`,`p-1 list-group-item rounded-0 bg-dan`)
+    } else if (uvin >= 6){
+        currentUVIndex.attr(`class`,`p-1 list-group-item rounded-0 bg-ora`)
+    } else if (uvin >= 3){
+        currentUVIndex.attr(`class`, `p-1 list-group-item rounded-0 bg-war`)
+    } else if (uvin >= 0){
+        currentUVIndex.attr(`class`, `p-1 list-group-item rounded-0 bg-suc`)
+    } 
 
     // append all list s into my UL and finally append that to the prerendered div currentCityStats
     ul.append(headerName, dateDisplay, currentTemp,currentHumidity,currentWindspeed, currentUVIndex)
     currentCityStats.append(ul)
 }
+function printForecastData(){
+    forecastDiv.empty()
+    var iconImg, temp, humidity, date, div;
+    for(k=0;k<currentCity.forecast.length;k++){
+        date = moment().add(1+k,`days`).format('l')
+        div = $(`<div>`).attr(`class`,`float-left text-left bg-ora m-2 p-3`)
+        iconImg = $(`<img>`).attr(`src`, currentCity.forecast[k].icon_url).attr(`class`,`mx-3 icon-image`)
+        temp = $(`<p>`).attr(`class`,`p-1`).html(`<span class="font-weight-bold">Temp</span>: `+currentCity.forecast[k].temp)
+        humidity = $(`<p>`).attr(`class`,`p-1`).html(`<span class="font-weight-bold">humidity</span>: `+currentCity.forecast[k].humidity)
+        div.append(date,iconImg,temp,humidity)
+        forecastDiv.append(div)
+    }
+}
+// function pullFiveDayForecast(){
+    
+// }
 function getGoogleImages(){
     console.log(currentCity.name.length)
     // for(i=0;i<currentCity.name.length;i++){
 
     // }
-    
-    
-
 }
 
+// re-run search if any of "history" searches is clicked
+historyDiv.on(`click`,function(e){
+    var el = e.target
+    if(el.attributes[`data-isListItem`].value === `y`){
+        searchTerm = el.innerText
+        pullWeatherData()
+    } else{
+        console.log(`I'm sorry, that's not a list item`)
+    }
+})
+console.log(`======= END OF SCRIPT =======`)
 console.log(currentCity)
